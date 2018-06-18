a2186c0f4bf89d5913adb2584e6b58c466750da5
Juan Antonio Reyes <biballo146@gmail.com>
2018-06-18 04:40:54 +0200

    Pokemon list filter by stats (Min and max)

--- src/app/filter-pokemon.pipe.ts
+++ src/app/filter-pokemon.pipe.ts
@@ -6,13 +6,104 @@ import { Pokemon } from './pokemon';
 })
 export class FilterPokemonPipe implements PipeTransform {
 
-  transform(value: Pokemon[], nameInput: string): Pokemon[] {
-    nameInput = nameInput.trim().toUpperCase();
+	// UGLY PATCH? - TRY TO FIND A BETTER SOLUTION?
+	// The "elements" parameter is passed to force the pipe to be called when
+	// we add pokemon to the list with push (The pure pipe needs a pure change
+	// to be called, like the "elements" value that changes with the list length)
+	// So the pokemon will be visible and sorted even if we see them wile they 
+	// are being pushed into the list
+	transform(value: Pokemon[],
+						name: string,
+						minHp: number, maxHp: number,
+						minAttack: number, maxAttack: number,
+						minDefense: number, maxDefense: number,
+						minSpAttack: number, maxSpAttack: number,
+						minSpDefense: number, maxSpDefense: number,
+						minSpeed: number, maxSpeed: number,
+						elements: number): Pokemon[] {
+		
+		let filteredPokemon: Pokemon[];
 
-		return value.filter(pokemon => {
-			return ( pokemon.name.toUpperCase().includes(nameInput) );
-		});
+		// Filter by name
+		name = name.trim().toUpperCase();
 
-  }
+		filteredPokemon = value.filter(pokemon => {
+												return ( pokemon.name.toUpperCase().includes(name) );
+											});
+
+		// Filter by HP
+		if ( (!isNaN(minHp) && (minHp !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.hp >= minHp );
+												});
+		}
+		if ( (!isNaN(maxHp) && (maxHp !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.hp <= maxHp );
+												});
+		}
+
+		// Filter by attack
+		if ( (!isNaN(minAttack) && (minAttack !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.attack >= minAttack );
+												});
+		}
+		if ( (!isNaN(maxAttack) && (maxAttack !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.attack <= maxAttack );
+												});
+		}
+
+		// Filter by defense
+		if ( (!isNaN(minDefense) && (minDefense !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.defense >= minDefense );
+												});
+		}
+		if ( (!isNaN(maxDefense) && (maxDefense !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.defense <= maxDefense );
+												});
+		}
+
+		// Filter by special attack
+		if ( (!isNaN(minSpAttack) && (minSpAttack !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.spAttack >= minSpAttack );
+												});
+		}
+		if ( (!isNaN(maxSpAttack) && (maxSpAttack !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.spAttack <= maxSpAttack );
+												});
+		}
+
+		// Filter by special defense
+		if ( (!isNaN(minSpDefense) && (minSpDefense !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.spDefense >= minSpDefense );
+												});
+		}
+		if ( (!isNaN(maxSpDefense) && (maxSpDefense !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.spDefense <= maxSpDefense );
+												});
+		}
+
+		// Filter by speed
+		if ( (!isNaN(minSpeed) && (minSpeed !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.speed >= minSpeed );
+												});
+		}
+		if ( (!isNaN(maxSpeed) && (maxSpeed !== null) ) {
+			filteredPokemon = filteredPokemon.filter(pokemon => {
+													return ( pokemon.baseStats.speed <= maxSpeed );
+												});
+		}
+
+		return filteredPokemon;
+	}
 
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -12,7 +12,22 @@ export class PokemonListComponent implements OnInit {
 
 	pokemonList: Pokemon[] = [];
 
+  // Filters
+  // They need to be in separate variables for making the filter pipe calls
+  // when the bounded ngModel values are changed
   nameFilter: string = null;
+  minHp: number = null;
+  maxHp: number = null;
+  minAttack: number = null;
+  maxAttack: number = null;
+  minDefense: number = null;
+  maxDefense: number = null;
+  minSpAttack: number = null;
+  maxSpAttack: number = null;
+  minSpDefense: number = null;
+  maxSpDefense: number = null;
+  minSpeed: number = null;
+  maxSpeed: number = null;
 
   sortListBy: string = null;
   reverseList: number = null;
@@ -39,6 +54,22 @@ export class PokemonListComponent implements OnInit {
   changeListSorting(sortListBy): void {
     this.sortListBy = sortListBy;
     this.reverseList *= -1;
+  }
+
+  clearFilters(): void {
+    this.nameFilter = '';
+    this.minHp = null;
+    this.maxHp = null;
+    this.minAttack = null;
+    this.maxAttack = null;
+    this.minDefense = null;
+    this.maxDefense = null;
+    this.minSpAttack = null;
+    this.maxSpAttack = null;
+    this.minSpDefense = null;
+    this.maxSpDefense = null;
+    this.minSpeed = null;
+    this.maxSpeed = null;
   }
 
 }
